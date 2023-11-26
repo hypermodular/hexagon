e7173eceae06cc737a6c7b7ed66a00cd592ba4f7
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-11-26 11:08:09 +0000

    update

--- hexagon.js
+++ hexagon.js
@@ -5,24 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
 function getHexagonPoints(edgeLength) {
     // Oblicz wysokość sześciokąta
     const height = Math.sqrt(3) * edgeLength;
-    const width = Math.sqrt(3) * edgeLength;
+    //const width = Math.sqrt(3) * edgeLength;
     const centerY = height / 2 + edgeLength / 2;
-    const centerX = width / 2 + edgeLength / 2;
+    //const centerX = width / 2 + edgeLength / 2;
 
     // Oblicz współrzędne wierzchołków
     const points = [
-        //{ x: edgeLength / 2, y: centerY }, // lewy górny wierzchołek
-        { x: edgeLength / 2, y: centerY }, // lewy górny wierzchołek
+        { x: edgeLength / 2, y: centerY }, // lewy górny wierzchołek        
         { x: 0, y: centerY - height / 2 }, // górny lewy
         { x: edgeLength, y: centerY - height / 2 }, // górny prawy
         { x: edgeLength * 1.5, y: centerY }, // prawy górny wierzchołek
-        { x: edgeLength, y: centerY + height / 2 }, // dolny prawy
-        { x: edgeLength, y: centerY + height / 2 }, // dolny lewy
-        //{ x: 0, y: centerY + height / 2 }, // dolny lewy
+        { x: edgeLength, y: centerY + height / 2 }, // dolny prawy        
+        { x: 0, y: centerY + height / 2 }, // dolny lewy
     ];
 
     // Połącz współrzędne w string dla atrybutu 'points' w SVG
-    return points.map(p => `${ Math.round(p.x + 'e+2') },${ Math.round(p.y + 'e+2') }`).join(' ');
+    return points.map(p => `${ Math.round(p.x) },${ Math.round(p.y) }`).join(' ');
 }
 
 function renderHexagon(edgeLength) {
