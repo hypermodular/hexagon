c6c6980163b9e144abef634aa9fbc3a02156a107
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-11-26 11:45:16 +0100

    dynamiczny bok

--- hexagon.js
+++ hexagon.js
@@ -1,25 +1,37 @@
-// Inicjalizacja SVG przy ładowaniu strony
 document.addEventListener('DOMContentLoaded', function() {
-    renderHexagon();
+    renderHexagon(50); // wartość początkowa dla długości boku
 });
 
-function renderHexagon(coords = "0,130 75,30 225,30 300,130 225,230 75,230") {
-    // Pobierz kontener dla SVG
-    const container = document.getElementById('svg-container');
+function getHexagonPoints(edgeLength) {
+    // Oblicz wysokość sześciokąta
+    const height = Math.sqrt(3) * edgeLength;
   
-    // Stwórz SVG za pomocą danych współrzędnych
-    container.innerHTML = `
+    // Oblicz współrzędne wierzchołków
+    const points = [
+        { x: 150 - edgeLength, y: 130 }, // lewy górny wierzchołek
+        { x: 150 - edgeLength / 2, y: 130 - height / 2 }, // górny lewy
+        { x: 150 + edgeLength / 2, y: 130 - height / 2 }, // górny prawy
+        { x: 150 + edgeLength, y: 130 }, // prawy górny wierzchołek
+        { x: 150 + edgeLength / 2, y: 130 + height / 2 }, // dolny prawy
+        { x: 150 - edgeLength / 2, y: 130 + height / 2 }, // dolny lewy
+    ];
+  
+    // Połącz współrzędne w string dla atrybutu 'points' w SVG
+    return points.map(p => `${p.x},${p.y}`).join(' ');
+}
+
+function renderHexagon(edgeLength) {
+    const hexagonPoints = getHexagonPoints(edgeLength);
+  
+    document.getElementById('svg-container').innerHTML = `
     <svg width="300px" height="260px" viewBox="0 0 300 260" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="${coords}" fill="none" stroke="black" />
-        <!-- Reszta figury zgodnie z podanymi współrzędnymi i właściwościami -->
+        <polygon points="${hexagonPoints}" fill="none" stroke="black" />
+        <!-- Tutaj powinny zostać dodane pozostałe figury i tekst -->
     </svg>
     `;
 }
 
 function updateHexagon() {
-    // Pobierz nowe współrzędne z input
-    const newCoords = document.getElementById('hexagon-coords').value;
-  
-    // Wyrenderuj sześciokąt z nowymi współrzędnymi
-    renderHexagon(newCoords);
+    const edgeLength = document.getElementById('edge-length').value;
+    renderHexagon(edgeLength);
 }
