f884f550d6999fea634ec9b05a232189b2c4d3ba
Tom Sapletta <tom-sapletta-com@users.noreply.github.com>
2023-11-26 11:39:56 +0100

    color

--- hexagon.js
+++ hexagon.js
@@ -10,7 +10,7 @@ function renderHexagon(coords = "0,130 75,30 225,30 300,130 225,230 75,230") {
     // Stwórz SVG za pomocą danych współrzędnych
     container.innerHTML = `
     <svg width="300px" height="260px" viewBox="0 0 300 260" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="${coords}" fill="none" stroke="white" />
+        <polygon points="${coords}" fill="none" stroke="black" />
         <!-- Reszta figury zgodnie z podanymi współrzędnymi i właściwościami -->
     </svg>
     `;
